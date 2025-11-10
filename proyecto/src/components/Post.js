7b94ae871d13d773e86c875710a6e827d8c7d20b
ddukss <pignib@udesa.edu.ar>
2025-11-09 22:43:33 -0300

    comenzando con post

---
+++ proyecto/src/components/Post.js
@@ -0,0 +1,4 @@
+import React, { Component } from "react";
+import { StyleSheet, Pressable, Text, View } from "react-native";
+import { db, auth } from '../firebase/config'
+import firebase from "firebase"; 
